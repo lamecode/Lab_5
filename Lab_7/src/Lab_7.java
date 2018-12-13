203f1c0ee767e579796da49a3df6102f7100180c
lamecode <44145440+lamecode@users.noreply.github.com>
2018-12-14 01:10:58 +0200

    Update Lab_7.java

--- Lab_7/src/Lab_7.java
+++ Lab_7/src/Lab_7.java
@@ -8,42 +8,62 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Lab_7 {
-    public static void main(String[] args) {
-        Scanner Lab_7 = new Scanner(System.in);
-        List<Integer> An = new ArrayList<>();
-        List<Integer> Bn = new ArrayList<>();
-        System.out.println("Введіть розмірність масиву:");
-        int n1 = Lab_7.nextInt();
-        int d =0;
-        int bn;
-        for (int k = 1; k<=n1; k++) {
-            int n = (int) (Math.random()*100);
+    private static void FillingArrayA (int n1, List<Integer> An) {
+        for (int k = 1; k <= n1; k++) {
+            int n = (int) (Math.random() * 100);
             An.add(n);
         }
-        int amax = An.get(0);
+    }
+
+    private static void SearchingForMax (int amax, List<Integer> An) {
         for (int i = 1; i < An.size(); i++) {
             if (An.get(i) > amax) {
                 amax = An.get(i);
             }
         }
-        for (int s = 0; s<n1; s++) {
-            bn = amax - An.get(s);
+    }
+
+    private static void FillingArrayB (int n1, int amax, List<Integer> An, List<Integer> Bn) {
+        for (int s = 0; s < n1; s++) {
+            int bn = amax - An.get(s);
             Bn.add(bn);
         }
-        for ( int e = 0; e<n1; e++) {
+    }
+
+    private static void ValueOfD (int n1, int d, List<Integer> An, List<Integer> Bn) {
+        for ( int e = 0; e < n1; e++) {
             d += Math.pow(An.get(e),2) - Math.pow(Bn.get(e),2);
         }
-        for (int c = 0; c<n1; c++){
+    }
+
+    private static void ChangingValueOfElements (int n1, List<Integer> An, int d) {
+        for (int c = 0; c < n1; c++){
             if (An.get(c)%7 == 0) {
                 int n11 = An.get(c) + d;
                 An.set(c, n11);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner Lab_7 = new Scanner(System.in);
+        List<Integer> An = new ArrayList<>();
+        List<Integer> Bn = new ArrayList<>();
+        System.out.println("Введіть розмірність масиву:");
+        int d;
+        int n1 = Lab_7.nextInt();
+        FillingArrayA (n1, An);
+        int amax = An.get(0);
+        SearchingForMax (amax, An);
         System.out.println("Масив An:" + "\n" + An);
         System.out.print("Максимальне значення масиву Аn - " + amax + "\n");
         System.out.println("Масив Вn:");
+        FillingArrayB (n1, amax, An, Bn);
         System.out.println(Bn);
         System.out.println("Значення d:");
+        ValueOfD (n1, An, d, Bn);
+        ChangingValueOfElements (n1, An, d);
         System.out.println(d);
     }
+
 }
